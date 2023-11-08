4eeb3f47629fe843f60e0213b7d877f729f1b819
samanthablazizza <samantha.blazizza@gmail.com>
2023-11-08 12:11:47 -0300

    Listar, editar e deletar componentes

--- blogpessoal_front/src/components/navbar/Navbar.tsx
+++ blogpessoal_front/src/components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
 
     const navigate = useNavigate()
     const { handleLogout } = useContext(AuthContext)
-  
+
     function logout() {
         handleLogout()
         alert('Usuário deslogado com sucesso')
@@ -18,20 +18,20 @@ function Navbar() {
         <>
             <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
-            
-            <div className="container flex justify-between text-lg">
-	            <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
-	
-            <div className='flex gap-4'>
-		        Postagens |
-		        <Link to='/temas' className='hover:underline'>Temas</Link>
-		        <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
-		        Perfil |
-		        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
+                        Perfil 
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
+        </>
     )
 }
 
--- blogpessoal_front/src/pages/home/Home.tsx
+++ blogpessoal_front/src/pages/home/Home.tsx
@@ -1,6 +1,8 @@
+import ListaPostagens from "../../components/postagens/listaPostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalPostagem/ModalPostagem"
+
 function Home() {
-    
-     return (
+    return (
         <>
             <div className="bg-indigo-900 flex justify-center">
                 <div className='container grid grid-cols-2 text-white'>
@@ -13,10 +15,8 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                                >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
@@ -30,6 +30,8 @@ function Home() {
                     </div>
                 </div>
             </div>
+
+            <ListaPostagens />
         </>
     )
 }
